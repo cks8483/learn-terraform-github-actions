12476b6e1568d87d0c0e37fe3d7663f9a63bc003
ChanHoe Choi <50437623+cks8483@users.noreply.github.com>
2022-05-30 16:50:58 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
+      version = "3.75.2"
     }
   }
   required_version = ">= 1.1.0"
